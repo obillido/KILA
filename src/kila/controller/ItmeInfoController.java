31c842f07b7b877615f5bd9add3479113360c75f
kimyungi <seebud@naver.com>
2019-10-10 11:49:15 +0900

    iteminfocon

--- src/kila/controller/ItmeInfoController.java
+++ src/kila/controller/ItmeInfoController.java
@@ -3,14 +3,16 @@ package kila.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/iteminfo")
 public class ItmeInfoController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect(req.getContextPath()+"/index.jsp");
+		resp.sendRedirect(req.getContextPath()+"/layout.jsp?cpage=/kimyungi/ItemInfo.jsp");
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
