3a0f3f2bc404346248c46dadf4e83c0057d23ee5
yhkim <obillido@naver.com>
2019-10-11 12:11:49 +0900

    productInfo getList

--- src/kila/dao/ProductInfoDao.java
+++ src/kila/dao/ProductInfoDao.java
@@ -20,7 +20,7 @@ public class ProductInfoDao {
 		Connection con=null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
-		String sql="select * from product_name p,color c,product d,product_reg r where p.pcode=c.pcode and d.colnum=c.colnum and r.pnum=d.pnum and p.pcode=? and c.colnum=?";
+		String sql="select * from product_name p,color c, product d,product_reg r where p.pcode=c.pcode and d.colnum=c.colnum and r.pnum=d.pnum and p.pcode=? and c.colnum=?";
 		try {
 			con=JdbcUtil.getConn();
 			pstmt=con.prepareStatement(sql);
@@ -66,4 +66,72 @@ public class ProductInfoDao {
 			JdbcUtil.close(con,pstmt,rs);
 		}
 	}
+
+	
+
+	public ArrayList<ProductInfoVo> getList(){
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=JdbcUtil.getConn();
+			String sql="select pn.pcode, cname, pname, price, color, savefilename, psize, icnt from product_name pn, color, product " + 
+						"where pn.pcode=color.pcode and color.colnum=product.colnum " + 
+						"order by pcode, pname, color, psize";
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			ArrayList<ProductInfoVo> list=new ArrayList<ProductInfoVo>();
+			while(rs.next()) {
+				list.add(new ProductInfoVo(
+						rs.getString("pcode"), 
+						rs.getString("cname"), 
+						rs.getString("pname"), 
+						rs.getInt("price"), 
+						rs.getString("color"),
+						rs.getString("savefilename"),
+						rs.getString("psize"), 
+						rs.getInt("icnt")));
+			}
+			return list;
+		}catch(SQLException se) {
+			System.out.println("ProductDAO:list:"+se.getMessage());
+			return null;
+		}finally {
+			JdbcUtil.close(con,pstmt,rs);
+		}
+	}
+	
+	public ArrayList<ProductInfoVo> getList(String category){
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=JdbcUtil.getConn();
+			String sql="select pn.pcode, cname, pname, price, color, savefilename, psize, icnt from product_name pn, color, product " + 
+						"where pn.pcode=color.pcode and color.colnum=product.colnum and cname=" +category+" "+ 
+						"order by pcode, pname, color, psize";
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			ArrayList<ProductInfoVo> list=new ArrayList<ProductInfoVo>();
+			while(rs.next()) {
+				list.add(new ProductInfoVo(
+						rs.getString("pcode"), 
+						rs.getString("cname"), 
+						rs.getString("pname"), 
+						rs.getInt("price"), 
+						rs.getString("color"),
+						rs.getString("savefilename"),
+						rs.getString("psize"), 
+						rs.getInt("icnt")));
+			}
+			return list;
+		}catch(SQLException se) {
+			System.out.println("ProductDAO:list:"+se.getMessage());
+			return null;
+		}finally {
+			JdbcUtil.close(con,pstmt,rs);
+		}
+	}
+	
+	
 }
