3a40b165de0d4a8c0de3a7ed4f6c63f08a2b7292
kimyungi <seebud@naver.com>
2019-10-15 14:28:35 +0900

    결제수정중

--- src/kila/controller/PaymentController.java
+++ src/kila/controller/PaymentController.java
@@ -7,10 +7,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import org.apache.catalina.startup.SetAllPropertiesRule;
 
+import kila.dao.BuyerDao;
 import kila.dao.ItemInfoDao;
+import kila.vo.BuyerVo;
 import kila.vo.ItemInfoSizeVo;
 import kila.vo.ItemInfoVo;
 
@@ -21,9 +25,14 @@ public class PaymentController extends HttpServlet{
 		int colnum=Integer.parseInt(req.getParameter("scolnum"));
 		int psize=Integer.parseInt(req.getParameter("spsize"));
 		int cnt=Integer.parseInt(req.getParameter("pcnt"));
+		HttpSession session=req.getSession(); 
+		String id=(String)session.getAttribute("id");
 		ItemInfoDao dao=ItemInfoDao.getInstance();
+		BuyerDao dao2=new BuyerDao();
 		ItemInfoVo vo=dao.productInfos(colnum);
+		BuyerVo vo2=dao2.getInfo(id);
 		req.setAttribute("vo", vo);
+		req.setAttribute("vo2", vo2);
 		req.setAttribute("psize",psize);
 		req.setAttribute("cnt", cnt);
 		req.getRequestDispatcher("/kimyungi/result.jsp").forward(req,resp);
